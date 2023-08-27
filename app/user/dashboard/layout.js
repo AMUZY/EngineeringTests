cd2891b061bbc35f5acb4fc22a20ee980d308f7b
amuzy <nnamdiamu5@gmail.com>
2023-08-27 07:57:04 +0100

    Finally done 4

--- app/user/dashboard/layout.js
+++ app/user/dashboard/layout.js
@@ -6,27 +6,13 @@ import { useRouter } from "next/navigation";
 import { getSession } from "next-auth/react";
 
 export default function layout ({ children }) {
-  const [authstate,setAuthState] = useState(false)
-  const router = useRouter()
-  function CheckAuth(){
-    const session = getSession()
-    session.then((status)=>{
-      if(!status){
-        router.push("/unauthenticated")
-      }else{
-        setAuthState(true)
-      }
-    }).catch(()=>{})
-  }
-  CheckAuth()
-
   const [localstore,setLocalStore] = useState()
   useEffect(()=>{
     setLocalStore(localStorage)
   },[])
   
   return (
-    (authstate && localstore) &&
+    localstore &&
       <div className="w-full h-full p-6">
         <div className="w-full boxcontainer h-full flex flex-row rounded-3xl overflow-visible">
           <div className="mr-3 w-[20%] h-full dashbox rounded-3xl p-3">
